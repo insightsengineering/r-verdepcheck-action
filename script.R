0977996f4d6817fb7f27d5dfa447fe5d6117cd43
André Veríssimo <211358+averissimo@users.noreply.github.com>
2024-06-04 14:03:01 +0200

    Revert "feat: break action in multiple steps with debug"
    This reverts commit 4350b23ca9ce20b31468f2ac18d44772dff84d7c.

--- script.R
+++ script.R
@@ -38,6 +38,77 @@ fun <- switch(
 x <- fun(path, check_args = check_args, build_args = build_args)
 saveRDS(x, "res.RDS")
 
+cli::cli_h1("Debug output:")
+
+cli::cli_h2("Installation proposal:")
+x$ip
+
+cli::cli_h2("Installation proposal config:")
+x$ip$get_config()
+
+cli::cli_h2("Package DESCRIPTION file used (see Remotes section):")
+catnl(readLines(gsub(".*::", "", x$ip$get_refs())))
+
+cli::cli_h2("Dependency solution:")
+x$ip$get_solution()
+
+cli::cli_h2("Dependency resolution:")
+x_res <- subset(x$ip$get_resolution(), , c(ref, package, version, mirror, sources))
+if ("tibble" %in% rownames(installed.packages())) {
+    print(x_res, n = Inf)
+} else {
+    print(as.data.frame(x_res))
+}
+
+cli::cli_h2("Dependency resolution (tree):")
+try(x$ip$draw())
+
+# TODO: https://github.com/r-lib/pkgdepends/issues/305 - remove when fixed
+# this provides additional debug info in case of empty error report
+if (inherits(x$ip, "pkg_installation_proposal") &&
+    inherits(x$ip$get_solution(), "pkg_solution_result") &&
+    x$ip$get_solution()$status == "FAILED" &&
+    inherits(x$ip$get_solution()$failures, "pkg_solution_failures") &&
+    grepl("*.dependency conflict$", format(x$ip$get_solution()$failures)[[1]])
+) {
+    cli::cli_h2("Supplementary solution (experimental):")
+    xx <- pkgdepends::new_pkg_deps(
+        trimws(strsplit(
+            desc::desc(
+                gsub("deps::", "", x$ip$get_refs())
+            )$get_field("Config/Needs/verdepcheck")
+        , ",")[[1]]),
+        config = list(library = tempfile())
+    )
+    verdepcheck:::solve_ip(xx)
+    xx$get_solution()
+}
+
+cli::cli_h1("Create lockfile...")
+try(x$ip$create_lockfile("pkg.lock"))
+
+
+cli::cli_h1("R CMD CHECK:")
+x$check
+
+cli::cli_h2("R CMD CHECK status:")
+catnl(x$check$status)
+
+cli::cli_h2("R CMD CHECK install out:")
+cat(x$check$install_out)
+
+cli::cli_h2("R CMD CHECK stdout:")
+cat(x$check$stdout)
+
+cli::cli_h2("R CMD CHECK stderr:")
+cat(x$check$stderr)
+
+cli::cli_h2("R CMD CHECK session info:")
+x$check$session_info
+
+cli::cli_h2("R CMD CHECK test output:")
+lapply(x$check$test_output, cat)
+
 catnl()
 
 stopifnot("pkg dependency resolve failed - please see the above logs for details" = x$ip$get_solution()$status == "OK")
